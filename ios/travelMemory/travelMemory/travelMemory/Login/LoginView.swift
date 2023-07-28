432e72b8a4629369dfa5c8ddf9c0ec4df8acd8de
hyhwaxung <hyhwaxung@gmail.com>
2023-07-29 01:56:28 +0900

    2023.07.29 네이버, 카카오 로그인 버튼 구현

--- ios/travelMemory/travelMemory/travelMemory/Login/LoginView.swift
+++ ios/travelMemory/travelMemory/travelMemory/Login/LoginView.swift
@@ -10,6 +10,8 @@ import UIKit
 class LoginView: UIView {
     
     public let logoImage = UIImageView(image: UIImage(named: "appLogo"))
+    public let naverButton = NaverButton()
+    public let kakaoButton = KakaoButton()
     
     init() {
         super.init(frame: .zero)
@@ -18,9 +20,48 @@ class LoginView: UIView {
         setupView(element: logoImage, top: 300, centerX: 0)
         logoImage.widthAnchor.constraint(equalToConstant: 150).isActive = true
         logoImage.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        setupView(element: naverButton, left: 50)
+        naverButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 75).isActive = true
+        
+        setupView(element: kakaoButton)
+        kakaoButton.leftAnchor.constraint(equalTo: naverButton.rightAnchor, constant: 25).isActive = true
+        kakaoButton.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 75).isActive = true
     }
     
     required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class NaverButton: UIButton {
+    
+    init(){
+        super.init(frame: .zero)
+        setBackgroundImage(UIImage(named: "naverButton"), for: .normal)
+        
+        widthAnchor.constraint(equalToConstant: 50).isActive = true
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class KakaoButton: UIButton {
+    
+    init(){
+        super.init(frame: .zero)
+        setBackgroundImage(UIImage(named: "kakaoButton"), for: .normal)
+        
+        widthAnchor.constraint(equalToConstant: 50).isActive = true
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
