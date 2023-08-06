c2191f86044b517ae213fe9da2f680e7a18b0fbd
hyhwaxung <hyhwaxung@gmail.com>
2023-08-07 01:43:55 +0900

    2023.08.06 RegisterModel 정의

--- ios/travelMemory/travelMemory/Register/RegisterModel.swift
+++ ios/travelMemory/travelMemory/Register/RegisterModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct Post {
+    
+    var userId: String
+    var content: String
+    var viewCount: Int
+    var placeName: String
+    var dateTime: Date
+    var latitude: Double
+    var longitude: Double
+    var insertYMD: Date
+    var modYMD: Date
+    
+    init(userId: String, content: String, viewCount: Int, placeName: String, dateTime: Date, latitude: Double, longitude: Double, insertYMD: Date, modYMD: Date) {
+        self.userId = userId
+        self.content = content
+        self.viewCount = viewCount
+        self.placeName = placeName
+        self.dateTime = dateTime
+        self.latitude = latitude
+        self.longitude = longitude
+        self.insertYMD = insertYMD
+        self.modYMD = modYMD
+    }
+}
